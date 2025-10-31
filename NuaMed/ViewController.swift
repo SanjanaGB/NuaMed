6f7008aa1cc714c8fe01e34d780d5d07eb7a5672
sanjanagb <sanjana051198@gmail.com>
2025-10-31 14:10:56 -0400

    Initial Commit

---
+++ NuaMed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NuaMed
+//
+//  Created by Sanjana G B on 10/31/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NuaMedTests/NuaMedTests.swift
@@ -0,0 +1,17 @@
+//
+//  NuaMedTests.swift
+//  NuaMedTests
+//
+//  Created by Sanjana G B on 10/31/25.
+//
+
+import Testing
+@testable import NuaMed
+
+struct NuaMedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
